0171dc7f831915b6bb01557df955dbc7ab7f8d1f
kento89 <nerofuriku@gmail.com>
2021-08-07 08:43:26 +0000

    customer recruiter [show][edit]#3#4

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -5,6 +5,9 @@ class Customer < ApplicationRecord
          :recoverable, :rememberable, :validatable
 
   attachment :image
-  belongs_to :volunteer
+  belongs_to :volunteer, optional: true
   has_many :messages, dependent: :destroy
+  
+  enum experience: { '経験なし': 'false', '経験あり': 'true' }
+  enum volunteer_status: { 'ボランティア予定なし': 'false', 'ボランティア予定あり': 'true' }
 end
