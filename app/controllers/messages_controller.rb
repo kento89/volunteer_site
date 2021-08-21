ca3c685bc53969237731a5f161217aa00192ad51
kento89 <nerofuriku@gmail.com>
2021-08-21 10:56:12 +0000

    通知機能のみ　レイアウトなどはまだ行えていない

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,29 +1,38 @@
 class MessagesController < ApplicationController
 
   def create
+
     @message = Message.new(message_params)
+
     if current_customer != nil
       @message.customer = current_customer
     elsif current_recruiter != nil
       @message.recruiter = current_recruiter
     end
-    
+
     @room=@message.room
+
     if @message.save
-      @roommembernotme = Message.where(room_id: @room.id).where.not(recruiter_id: current_recruiter.id)  #自分以外の新しいメッセージをルーム内から取得
-      @theid = @roommembernotme.find_by(room_id: @room.id)
+
+      # @roommembernotme = Message.where(room_id: @room.id).where.not(recruiter_id: current_recruiter.id)  #自分以外の新しいメッセージをルーム内から取得
+      # @theid = @roommembernotme.find_by(room_id: @room.id)
       # 新しい通知
-      notification = Notification.new(room_id: @room.id, message_id: @message.id, customer_id: @message.custoemer, recruiter_id: currente_recruiter)
-      # 自分のメッセージは既読にする
-      if notification.recruiter_id == currente_recruiter.id
-        notification.checked = true
+      if @message.customer != nil
+        notification = Notification.new(visited_id: @message.room.volunteer.recruiter_id, visitor_id: @message.customer_id, room_id: @room.id, message_id: @message.id, checked: false)
+        notification.save
       end
-      notification.save if notification.valid?
-      
-      redirect_to request.referer
-    else
-      redirect_to request.referer
+
+      # 自分のメッセージは既読にする
+      # if notification.recruiter_id == currente_recruiter.id
+      #   notification.checked = true
+      # end
+
+      # notification.save if notification.valid?
+
+
     end
+    redirect_to request.referer
+
   end
 
   private
--- app/controllers/notifications_controller.rb
+++ app/controllers/notifications_controller.rb
@@ -1,16 +1,16 @@
 class NotificationsController < ApplicationController
-  
+
   def index
-    recruiter = current_recruiter.id
-    @notications = Notification.f() #自分への通知一覧を表示する
-    @customer = Notification.find_by(customer_id: params[:id])
+    # recruiter = current_recruiter.id
+    @notifications = Notification.where(visited_id: current_recruiter.id, checked: false)#自分への通知一覧を表示する
+    # @customer = Notification.find_by(customer_id: params[:id])
   end
-  
+
   def update
     notification=Notification.find(params[:id])
     if notification.update(checked: true)
       redirect_to action: :index
     end
   end
-  
+
 end
