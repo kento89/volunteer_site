ab2e63cf7873a4932074103cc8a0b972439634d4
kento89 <nerofuriku@gmail.com>
2021-08-22 11:43:30 +0000

    view[add]

--- app/controllers/recruiter/volunteers_controller.rb
+++ app/controllers/recruiter/volunteers_controller.rb
@@ -21,7 +21,7 @@ class Recruiter::VolunteersController < ApplicationController
   end
 
   def index
-    @volunteer = Volunteer.all
+    @volunteer = Volunteer.all.page(params[:page]).page(params[:page]).per(6)
     
   end
 
