cc5c85d1bb9f2597aad10e5a3866d126cebe4dcf
kento89 <nerofuriku@gmail.com>
2021-08-26 10:27:39 +0000

    [途中javascript]

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -119,7 +119,20 @@ $(document).on('turbolinks:load', function(){
     });
 });
 
-
+// top画面のスライド
+$(document).on('ready', function() {
+  $(".full").slick({
+    arrows: false,
+    dots: true,
+    autoplay: true,
+    autoplaySpeed: 1500,
+    speed: 1500,
+    fade: true,
+    pauseOnFocus: false,
+    pauseOnHover: false,
+    pauseOnDotsHover: false,
+  });
+});
 
 $(document).on('tubolinks:load',function(){
   $('#go_chat').on('click', function(){
