90bb6b46e85af299341c80e07e0fa1994fcea2c9
kento89 <nerofuriku@gmail.com>
2021-08-29 02:53:31 +0000

    バリデーション修正

--- app/controllers/recruiter/customers_controller.rb
+++ app/controllers/recruiter/customers_controller.rb
@@ -3,7 +3,6 @@ class Recruiter::CustomersController < ApplicationController
   end
   
   def show
-    @volunteer = Volunteer.find(params[:id])
-    @volunteer_customers = VolunteerCustomer.where(volunteer_id: @volunteer.id)
+    @customer = Customer.find(params[:id])
   end
 end
